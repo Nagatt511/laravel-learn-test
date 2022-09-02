a8dbcc2c74444913797d5d1e6fdcbe0b0ffd055b
Pyvovarov Danylo <dan4ishka5121@gmail.com>
2022-09-02 19:08:19 +0200

    created connetions to products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+
+   
+    
+    protected $fillable=['title','slug','description', 'price','barcode','stock','cover'];
+    
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function gallery()
+    {
+        return $this->hasOne(Gallery::class);
+    }
+    use HasFactory; 
 }
