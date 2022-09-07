2aa0d8969c8cefdf6b57fb21377fbeaa24112fb6
Pyvovarov Danylo <dan4ishka5121@gmail.com>
2022-09-07 16:03:55 +0200

    add material design bootstrap 4 cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,32 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+    />
+
+    <!-- Google Fonts -->
+
+    <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+     rel="stylesheet"
+    />
+
+    <!-- MDB -->
+
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/#[[latestVersion]]#/mdb.min.css"
+    rel="stylesheet"
+    />
+
     <!-- Scripts -->
+    <!-- MDB -->
+    <script
+        type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/#[[latestVersion]]#/mdb.min.js"
+    ></script>
+    
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
