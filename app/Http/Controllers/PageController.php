b5e462c975b07a33157c23adb9fd4b5e3b8929d6
Pyvovarov Danylo <dan4ishka5121@gmail.com>
2022-09-09 17:48:45 +0200

    create default function index  on PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::productBy('create_at','desc')->take(12)->get();
+
+        return view('welcome', compact($products));
+    }
 }
