182108de879eb12e7f87840742169fa696c7878e
Pyvovarov Danylo <dan4ishka5121@gmail.com>
2022-09-09 11:57:14 +0200

    return before created CatalogControler and created CatalogController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+// use App\Http\
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
 
 Auth::routes();
